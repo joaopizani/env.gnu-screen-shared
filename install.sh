9712921260c63f4cd6c516df2bf760a761ebc1ea
João Paulo Pizani Flor <joaopizani@gmail.com>
2015-05-05 15:27:57 -0300

    fixed typo in variable name in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
 BINDIR="${HOME}/bin"
 
-mkdir -p "${HOMEDIR}"
+mkdir -p "${BINDIR}"
 ln -s -f -n "${DIR}/newkey.sh" "${BINDIR}/newkey-shared-screen"
 hash -r
 
